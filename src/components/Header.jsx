8615af44984e8fd0728b2fb91274232bb7740a65
Whitney Simpson <whitmoon4819@gmail.com>
2024-03-18 13:07:53 -0400

    added Footer and profile image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ export default function Header({currentPage, handlePageChange}) {
     return (
       <header>
         <h1>Whitney Simpson's Portfolio</h1>
+        <img src="./Portfolio/IMG-6564.jpg" alt="Profile Photo"/>
         <NavTabs  currentPage={currentPage} handlePageChange={handlePageChange} />
       </header>
     );
